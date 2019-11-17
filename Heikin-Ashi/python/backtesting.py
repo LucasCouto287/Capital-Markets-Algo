5d791e52a8d1fcd64483da61036e2fbbee3c1cc1
Nrup R Patel <nruppatel133@gmail.com>
2019-11-17 01:31:57 -0500

    Update backtesting.py

--- Heikin-Ashi/python/backtesting.py
+++ Heikin-Ashi/python/backtesting.py
@@ -3,4 +3,6 @@ import requests
 import json
 def heikenashi_calc (date,startTime,endTime,flag,Prev_HA_Open,Prev_HA_Close):
     if(flag == 0):
-yo
+  latest = response.content
+            latest = json.loads(latest)
+            latest = latest[0]
