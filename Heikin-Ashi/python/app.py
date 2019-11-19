aa227febd2fd48b3fce95c2cefaa208744c728c2
Nrup R Patel <nruppatel133@gmail.com>
2019-11-19 00:11:39 -0500

    Update app.py

--- Heikin-Ashi/python/app.py
+++ Heikin-Ashi/python/app.py
@@ -1,2 +1,16 @@
 import requests
 import json
+from twilio.rest import Client
+from datetime import datetime, timedelta
+from flask import Flask, request
+import schedule
+import time
+from threading import Thread
+app = Flask(__name__)
+global HA_Close_latest 
+HA_Close_latest = 0 
+global HA_Open_latest 
+HA_Open_latest = 0
+global temp
+temp = 0
+def sms():
