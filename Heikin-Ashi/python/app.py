5df26de927e5aa5120c3635d6ad93e01a3e9f53b
Nrup R Patel <nruppatel133@gmail.com>
2019-11-19 00:12:36 -0500

    Update app.py

--- Heikin-Ashi/python/app.py
+++ Heikin-Ashi/python/app.py
@@ -25,4 +25,12 @@ def sms():
     split_datetime_prev = utc_datetime_prev.strip(' ')
     time_utc_datetime_prev = split_utc_datetime_prev[1]
     response = requests.get('https://globalmetals.xignite.com/xGlobalMetals.json/GetBars?Symbol=XAU&Currency=USD&AsOfDate='+date_utc_datetime_current+'&StartTime='+time_utc_datetime_prev+'&EndTime='+time_utc_datetime_current+'&PriceType=Mid&TickPrecision=Minute&TickPeriods=120&_token=F1EA90FFBFF4462993E99968CE39F08C')
-  
+      if(response.status_code == 200):
+        latest = response.content
+        latest = json.loads(latest)
+        latest = latest[0]
+        if(latest["Open"]!=0 and latest["Close"]!=0 and latest["Low"]!=0 and latest["High"]!=0 ):
+            if(HA_Close_latest == 0):  
+                HA_Open_latest = (1320.978+1319.854) /2
+            else:
+
